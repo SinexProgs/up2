4dc443fc3d02bb87f9c5d8ee1cf1b8f368461545
SinexProgs <sinexpts234@gmail.com>
2025-05-23 03:23:14 +0700

    pr 4 task 2 wip functions 2

--- pr4/t2.py
+++ pr4/t2.py
@@ -1,11 +1,12 @@
 import sqlite3
 
 def add_new_ingredient(name, count):
-    cursor.execute("INSERT INTO ingredients (name, count) (?, ?)", (name, count))
+    cursor.execute("INSERT INTO ingredients (name, count) VALUES (?, ?)", (name, count))
+    connection.commit()
 
-    
+
 def add_new_drink(name, abv, cost, ingredients, count):
-    cursor.execute("INSERT INTO drinks (name, abv, cost, count) (?, ?, ?, ?)",
+    cursor.execute("INSERT INTO drinks (name, abv, cost, count) VALUES (?, ?, ?, ?)",
                    (name, abv, cost, count))
     drink_id = cursor.lastrowid
 
@@ -14,15 +15,45 @@ def add_new_drink(name, abv, cost, ingredients, count):
     VALUES (?, ?)
     """, ((drink_id, x) for x in ingredients))
 
+    connection.commit()
+
 
 def add_new_cocktail(name, cost, composition):
-    cursor.execute("INSERT INTO cocktails (name, cost) (?, ?)", (name, cost))
+    cursor.execute("INSERT INTO cocktails (name, cost) VALUES (?, ?)", (name, cost))
     cocktail_id = cursor.lastrowid
 
     cursor.executemany("""
     INSERT INTO cocktails_composition (cocktail_id, drink_id)
     VALUES (?, ?)
     """, ((cocktail_id, x) for x in composition))
+
+    connection.commit()
+
+
+def print_all_ingredients():
+    cursor.execute("SELECT * FROM ingredients")
+    for values in cursor.fetchall():
+        print(f"{values[0]}. {values[1]} (осталось {values[2]})")
+
+
+def print_all_drinks():
+    cursor.execute("SELECT * FROM drinks")
+    for values in cursor.fetchall():
+        print(f"""{values[0]}. {values[1]} (осталось {values[4]}):
+ - Крепость: {values[2]}%
+ - Цена: {values[3]}""")
+
+def print_all_cocktails():
+    cursor.execute("""
+    SELECT cocktails.id, cocktails.name, cocktails.cost, AVG(drinks.abv) 
+    FROM cocktails
+    LEFT JOIN cocktails_composition composition ON cocktails.id = composition.cocktail_id
+    LEFT JOIN drinks ON drinks.id = composition.drink_id
+    """)
+    for values in cursor.fetchall():
+        print(f"""{values[0]}. {values[1]}:
+ - Крепость: {values[3]}%
+ - Цена: {values[2]}""")
 
 
 connection = sqlite3.connect("i_love_drink.db")
