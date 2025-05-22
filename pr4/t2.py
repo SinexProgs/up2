e1528f2c262f898e9b8544faeb1c0e33a2974c36
SinexProgs <sinexpts234@gmail.com>
2025-05-23 01:57:29 +0700

    pr 4 task 2 wip functions 1

--- pr4/t2.py
+++ pr4/t2.py
@@ -1,5 +1,30 @@
 import sqlite3
 
+def add_new_ingredient(name, count):
+    cursor.execute("INSERT INTO ingredients (name, count) (?, ?)", (name, count))
+
+    
+def add_new_drink(name, abv, cost, ingredients, count):
+    cursor.execute("INSERT INTO drinks (name, abv, cost, count) (?, ?, ?, ?)",
+                   (name, abv, cost, count))
+    drink_id = cursor.lastrowid
+
+    cursor.executemany("""
+    INSERT INTO drinks_ingredients (drink_id, ingredient_id)
+    VALUES (?, ?)
+    """, ((drink_id, x) for x in ingredients))
+
+
+def add_new_cocktail(name, cost, composition):
+    cursor.execute("INSERT INTO cocktails (name, cost) (?, ?)", (name, cost))
+    cocktail_id = cursor.lastrowid
+
+    cursor.executemany("""
+    INSERT INTO cocktails_composition (cocktail_id, drink_id)
+    VALUES (?, ?)
+    """, ((cocktail_id, x) for x in composition))
+
+
 connection = sqlite3.connect("i_love_drink.db")
 cursor = connection.cursor()
 
@@ -34,14 +59,13 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS cocktails (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT,
-	cost REAL,
+	cost REAL
 )
 """)
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS cocktails_composition (
 	cocktail_id INTEGER,
 	drink_id INTEGER,
-	volume REAL,
 	
 	PRIMARY KEY (cocktail_id, drink_id),
 	FOREIGN KEY (cocktail_id) REFERENCES cocktails(id) ON DELETE CASCADE,
